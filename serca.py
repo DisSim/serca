e6b79e8cbde82c4df86baf341e0a38fff467a895
Birm <birm@rbirm.us>
2018-05-12 22:22:50 -0400

    start kafka

--- serca.py
+++ serca.py
@@ -1,39 +1,45 @@
 import flask
 import yaml
-import pika
+import kafka # https://github.com/dpkp/kafka-python
+import secrets
 
 # read config
 with open('serca.yml') as config:
     cnf = yaml.load(config)
     queue_cnf = cnf.get('queue', {})
-
-# queue (rabitmq) connection
-q_creds = pika.credentials.PlainCredentials(queue_cnf.get('username', 'guest'),
-                                            queue_cnf.get('password', 'guest'))
-q_params = pika.ConnectionParameters(queue_cnf.get('host', 'localhost'),
-                                     queue_cnf.get('port', 5672),
-                                     queue_cnf.get('path', '/'),
-                                     q_creds)
-# Helper functions
 """
 Add to the queue.
 the operation is the queue channel name
 """
 def q_add(body, operation):
-    with pika.BlockingConnection(q_params) as connection:
-        channel = connection.channel()
-        channel.queue_declare(queue=operation)
-        channel.basic_publish(routing_key=operation, body=body)
-
-"""
-Task status from queue
-"""
-# TODO
+    secret = secrets.token_hex(queue_cnf.get('secret_len', 32))
+    body['_secret'] = secret
+    with kafka.KafkaProducer(
+        bootstrap_servers=queue_cnf.get("host", "localhost"),
+        value_serializer=lambda v: json.dumps(v).encode(queue_cnf.get("encoding",'utf-8'))) as queue:
+    future = queue.send(operation, key, body)
+    res = future.get(timeout=queue_cnf['timeout', 10])
+    return {'topic': res.topic, 'partition': res.partition, 'offset': res.offset, '_secret'}
 
 """
 Get from queue iff key matches
 """
 # TODO
+def q_get(operation, offset, secret):
+    with kafka.KafkaConsumer(operation,
+                         bootstrap_servers=queue_cnf.get("host", "localhost")) as queue:
+        res = [message in queue where message.topic.decode(
+            queue_cnf.get("encoding",'utf-8'))==operation
+               and message.offet.decode(queue_cnf.get("encoding",'utf-8'))==offset]
+        if len(res) == 0:
+            return {}
+        else:
+            res.update({n: res[n].decode(queue_cnf.get("encoding",'utf-8')) for n in res.keys()})
+            res_secret = res.pop('_secret', None)
+            if res_secret == secret:
+                return res[0]
+            else:
+                return {}
 
 # Routes
 
