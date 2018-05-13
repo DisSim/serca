4cc51823b0f135d31364bbddb5af2a789991447e
Birm <birm@rbirm.us>
2018-05-12 21:45:17 -0400

    rabitmq version

--- serca.py
+++ serca.py
@@ -1,11 +1,29 @@
 import flask
-# TODO pick a queue
+import yaml
+import pika
 
+# read config
+with open('serca.yml') as config:
+    cnf = yaml.load(config)
+    queue_cnf = cnf.get('queue', {})
+
+# queue (rabitmq) connection
+q_creds = pika.credentials.PlainCredentials(queue_cnf.get('username', 'guest'),
+                                            queue_cnf.get('password', 'guest'))
+q_params = pika.ConnectionParameters(queue_cnf.get('host', 'localhost'),
+                                     queue_cnf.get('port', 5672),
+                                     queue_cnf.get('path', '/'),
+                                     q_creds)
 # Helper functions
 """
-Add to the queue
+Add to the queue.
+the operation is the queue channel name
 """
-# TODO
+def q_add(body, operation):
+    with pika.BlockingConnection(q_params) as connection:
+        channel = connection.channel()
+        channel.queue_declare(queue=operation)
+        channel.basic_publish(routing_key=operation, body=body)
 
 """
 Task status from queue
